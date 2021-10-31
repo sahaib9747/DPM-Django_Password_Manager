ac4b71ff452f85e771629d930a472aa3ecd729cf
sahaib9747 <pro.sahaib9747@gmail.com>
2021-10-31 20:04:27 +0600

    DB, Static, Templates path configured

--- DPM/settings.py
+++ DPM/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for DPM project.
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'DPM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'DPM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DPM',
+        'USER': 'admin',
+        'PASSWORD': 'rihan@KHAN01854487',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
 
@@ -118,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
