cd83514e8e96c9d6bd2aea45ab155585b251ed08
sahaib9747 <pro.sahaib9747@gmail.com>
2021-11-04 22:14:22 +0600

    Mail verification added

--- manager/views.py
+++ manager/views.py
@@ -11,49 +11,49 @@ from django.contrib import messages
 
 # Create your views here.
 
-# def send_email():
-#     email_subject = 'DPM verification'
-#     code = str(random.randint(10000, 999999))
-#     email_body = f"Your verification code is: {code}"
-#     to = "sahaib9747@yahoo.com"
-#     send_mail(
-#             email_subject,
-#             email_body,
-#             settings.EMAIL_HOST_USER,
-#             [to],
-#             fail_silently=False
-#         )
-#     return code
-# def ran():
-#     return random.random()
+def send_email():
+    email_subject = 'DPM verification'
+    code = str(random.randint(10000, 999999))
+    email_body = f"Your verification code is: {code}"
+    to = "sahaib9747@yahoo.com"
+    send_mail(
+            email_subject,
+            email_body,
+            settings.EMAIL_HOST_USER,
+            [to],
+            fail_silently=False
+        )
+    return code
+def ran():
+    return random.random()
 
-# rand = ran()
-# def index(request):
-#     #send_email()
-#     if request.method == 'POST':
-#         if 'login-form' in request.POST:
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             # check user
-#             login = authenticate(username=username, password=password)
-#             if login is None:
-#                 global security_code 
-#                 security_code = send_email()
-#             else:
-#                 print('none noy')
-#                 messages.error(request, 'Invalid credentials!')
-#                 return redirect('/')
-#         elif 'code-submit-form' in request.POST:
-#             code = request.POST['security-code']
-#             if code == security_code:
-#                 print('checked')
-#             else:
-#                 print('wrong code')
-#         else:
-#             print(security_code)
-#     print(rand)     
-#     print('Name', type(name))          
-#     return render(request, 'index.html')
+rand = ran()
+def index(request):
+    #send_email()
+    if request.method == 'POST':
+        if 'login-form' in request.POST:
+            username = request.POST['username']
+            password = request.POST['password']
+            # check user
+            login = authenticate(username=username, password=password)
+            if login is None:
+                global security_code 
+                security_code = send_email()
+            else:
+                print('none noy')
+                messages.error(request, 'Invalid credentials!')
+                return redirect('/')
+        elif 'code-submit-form' in request.POST:
+            code = request.POST['security-code']
+            if code == security_code:
+                print('checked')
+            else:
+                print('wrong code')
+        else:
+            print(security_code)
+    print(rand)     
+    print('Name', type(name))          
+    return render(request, 'index.html')
 print('This is manager.views')
 def another():
     global student 
